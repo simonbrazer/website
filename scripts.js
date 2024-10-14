7a6ca9dd434f0f168d33163069848e97346de02f
Simon <88495025+simonbrazer@users.noreply.github.com>
2024-10-14 19:52:25 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
+  
+    burger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  
+    document.querySelectorAll(".nav-links a").forEach((link) => {
+      link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+      });
+    });
+  });
